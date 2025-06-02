34ebd15e060c1e07d08c5c420fb3ca0fdd700899
Rishab Patidar <patidarrishab9@gmail.com>
2025-06-02 17:57:16 +0530

    err solution

--- DataBase/gms.sql
+++ DataBase/gms.sql
@@ -1,36 +1,56 @@
-create database GMS;
- use GMS;
- CREATE TABLE Diet (
+CREATE DATABASE GMS;
+USE GMS;
+
+CREATE TABLE Diet (
     Diet_Id INT AUTO_INCREMENT PRIMARY KEY,
     Type VARCHAR(20),
     Diet_Name VARCHAR(20),
     Calories INT,
-    Vegan VARCHAR(20) );
+    Vegan BOOLEAN
+);
+
 CREATE TABLE Ingredient (
-     Ingredient_Id INT AUTO_INCREMENT PRIMARY KEY,
-     Diet_Id INT,
-     Ingredient_Name VARCHAR(50),
-     FOREIGN KEY (Diet_Id) REFERENCES Diet(Diet_Id));
+    Ingredient_Id INT AUTO_INCREMENT PRIMARY KEY,
+    Diet_Id INT,
+    Ingredient_Name VARCHAR(50),
+    FOREIGN KEY (Diet_Id) REFERENCES Diet(Diet_Id) ON DELETE CASCADE
+);
+
 CREATE TABLE Exercise (
-        Id INT AUTO_INCREMENT PRIMARY KEY,
-        Type VARCHAR(20),
-        Exercise_Name VARCHAR(20),
-        Body_Type VARCHAR(20));
- create table trannie(trannie_id int Auto_increment primary key,
-        name varchar(20),email varchar(30),
-        password varchar(20),height DECIMAL(4,2),
-        weight DECIMAL(5,2),allergies varchar(20),
-        disablity varchar(20));
+    Exercise_Id INT AUTO_INCREMENT PRIMARY KEY,
+    Type VARCHAR(20),
+    Exercise_Name VARCHAR(20),
+    Body_Type VARCHAR(20)
+);
+
+CREATE TABLE Trainee (
+    Trainee_Id INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(20),
+    Email VARCHAR(30) UNIQUE,
+    Password VARCHAR(20),
+    Height DECIMAL(4,2),
+    Weight DECIMAL(5,2),
+    Allergies VARCHAR(50), 
+    Disability VARCHAR(50)
+);
+
 CREATE TABLE Disability (
-        Id INT PRIMARY KEY,
-        Body_Part VARCHAR(20),
-    FOREIGN KEY (Id) REFERENCES Trannie( trannie_id ));
-create table Category(
-    id int auto_increment primary key,
-        type varchar (20),
-        foreign key (id) references Trannie(trannie_id));
-create table Trainner(trainer_id int auto_increment primary key,
-        name varchar(20),
-        email varchar(20),
-        password varchar(20)
+    Disability_Id INT AUTO_INCREMENT PRIMARY KEY,
+    Body_Part VARCHAR(20),
+    Trainee_Id INT,
+    FOREIGN KEY (Trainee_Id) REFERENCES Trainee(Trainee_Id) ON DELETE CASCADE
+);
+
+CREATE TABLE Category (
+    Category_Id INT AUTO_INCREMENT PRIMARY KEY,
+    Type VARCHAR(20),
+    Trainee_Id INT,
+    FOREIGN KEY (Trainee_Id) REFERENCES Trainee(Trainee_Id) ON DELETE CASCADE
+);
+
+CREATE TABLE Trainer (
+    Trainer_Id INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(20),
+    Email VARCHAR(30) UNIQUE,
+    Password VARCHAR(20)
 );

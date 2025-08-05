4c76eb24b1de2e34657070df8e90fcffa6a023a1
Rishab Patidar <patidarrishab9@gmail.com>
2025-08-05 14:37:54 +0530

    this is product.js

--- Server/router/products.js
+++ Server/router/products.js
@@ -1,22 +1,72 @@
-const {apiSuccess, apiError, apiResult} = require("../utils/apiresult")
-const express = require("express")
-//const bcrypt = require("bcrypt")
-const router = express.Router()
-const db = require("../utils/dbpool")
+const { apiSuccess, apiError } = require("../utils/apiresult");
+const express = require("express");
+const router = express.Router();
 
-router.get("/prod/:id",(req,resp)=>{
-    db.query(`select p.name, p.type, p.price
-                from category c
-                join products p on p.type = c.type
-                where c.trainee_id = ?;
+router.get("/prod/:id", (req, resp) => {
+    db.query(
+        `SELECT p.name, p.type, p.price
+         FROM category c
+         JOIN products p ON p.type = c.type
+         WHERE c.trainee_id = ?;`,
+        [req.params.id],
+        (err, result) => {
+            if (err) return resp.send(apiError(err));
+            if (result.length === 0) return resp.send(apiError("No products found"));
+            return resp.send(apiSuccess(result));
+        }
+    );
+});
 
-`,[req.params.id],(err,result)=>{
-    if(err){
-        return resp.send(apiError(err))
-    }else{
-        return resp.send(apiResult(result))
-    }
-})
-})
+router.get("/find/:id", (req, resp) => {
+    db.query(
+        "SELECT * FROM products WHERE id = ?",
+        [req.params.id],
+        (err, result) => {
+            if (err) return resp.send(apiError(err));
+            if (result.length !== 1) return resp.send(apiError("Product not found"));
+            return resp.send(apiSuccess(result[0]));
+        }
+    );
+});
 
-module.exports=router;
+router.patch("/edit/:id", (req, resp) => {
+    const { name, type, price } = req.body;
+    db.query(
+        "UPDATE products SET name = ?, type = ?, price = ? WHERE id = ?",
+        [name, type, price, req.params.id],
+        (err, result) => {
+            if (err) return resp.send(apiError(err));
+            if (result.affectedRows !== 1) return resp.send(apiError("Update failed"));
+            return resp.send(apiSuccess("Product updated successfully"));
+        }
+    );
+});
+
+router.get("/bmi/:id", (req, resp) => {
+    db.query(
+        `SELECT p.name, p.type, p.price
+         FROM bmi b
+         JOIN category c ON b.trainee_id = c.trainee_id
+         JOIN products p ON p.type = c.type
+         WHERE b.trainee_id = ?`,
+        [req.params.id],
+        (err, result) => {
+            if (err) return resp.send(apiError(err));
+            return resp.send(apiSuccess(result));
+        }
+    );
+});
+
+router.get("/search/:name", (req, resp) => {
+    db.query(
+        "SELECT * FROM products WHERE name LIKE ?",
+        [`%${req.params.name}%`],
+        (err, result) => {
+            if (err) return resp.send(apiError(err));
+            if (result.length === 0) return resp.send(apiError("No matching products found"));
+            return resp.send(apiSuccess(result));
+        }
+    );
+});
+
+module.exports = router;
